1bce94e5b19f8de6e9d1d2041469e460ece8e4a9
Fakihelmi <helmifakih5@gmail.com>
2024-12-27 03:03:58 +0700

    disini di update sama hedi

--- src/main/java/Kelompok_1_tubes/main/main.java
+++ src/main/java/Kelompok_1_tubes/main/main.java
@@ -2,6 +2,10 @@ package Kelompok_1_tubes.main;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("hi from main");
+        int x = 5;
+        int y  = -10;
+
+        x = 10;
+        System.out.println(x+y);
     }
 }
